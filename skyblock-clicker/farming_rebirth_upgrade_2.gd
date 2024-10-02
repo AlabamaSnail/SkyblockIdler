fd70bb6f5ee4bef886fe321ddb9eb47682910883
AlabamaSnail <74316013+AlabamaSnail@users.noreply.github.com>
2024-10-02 16:47:19 -0500

    Rebirth Buttons

---
+++ skyblock-clicker/farming_rebirth_upgrade_2.gd
@@ -0,0 +1,19 @@
+extends Button
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.text = "Lvl. " + str(StatHolder.farmingRebirthUpgrade) + " Farming Skill x2 : " + str(StatHolder.farmingRebirthUpgrade ** 3 + 1) + "P"
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	self.text = "Lvl. " + str(StatHolder.farmingRebirthUpgrade) + " Farming Skill x2 : " + str(StatHolder.farmingRebirthUpgrade ** 3 + 1) + "P"
+
+func _on_pressed() -> void:
+	if StatHolder.rebirthPoints >= (StatHolder.farmingRebirthUpgrade ** 3 + 1):
+		StatHolder.rebirthPoints -= (StatHolder.farmingRebirthUpgrade ** 3 + 1)
+		StatHolder.farmingRebirthUpgrade += 1
+		self.text = "Lvl. " + str(StatHolder.farmingRebirthUpgrade) + " Farming Skill x2 : " + str(StatHolder.farmingRebirthUpgrade ** 3 + 1) + "P"
+	else:
+		self.disabled = true
+		await get_tree().create_timer(0.2).timeout
+		self.disabled = false
---
+++ skyblock-clicker/mining_rebirth_upgrade_4.gd
@@ -0,0 +1,19 @@
+extends Button
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.text = "Lvl. " + str(StatHolder.miningRebirthUpgrade) + " Mining Skill x2 : " + str(StatHolder.miningRebirthUpgrade ** 3 + 1) + "P"
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	self.text = "Lvl. " + str(StatHolder.miningRebirthUpgrade) + " Mining Skill x2 : " + str(StatHolder.miningRebirthUpgrade ** 3 + 1) + "P"
+
+func _on_pressed() -> void:
+	if StatHolder.rebirthPoints >= (StatHolder.miningRebirthUpgrade ** 3 + 1):
+		StatHolder.rebirthPoints -= (StatHolder.miningRebirthUpgrade ** 3 + 1)
+		StatHolder.miningRebirthUpgrade += 1
+		self.text = "Lvl. " + str(StatHolder.miningRebirthUpgrade) + " Mining Skill x2 : " + str(StatHolder.miningRebirthUpgrade ** 3 + 1) + "P"
+	else:
+		self.disabled = true
+		await get_tree().create_timer(0.2).timeout
+		self.disabled = false
